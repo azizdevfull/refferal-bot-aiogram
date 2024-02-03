2ab03b807b182eaeeb59574c99081b25cf88db58
Azizbek Isroilov <aziz16110904@gmail.com>
2024-02-03 09:14:00 +0500

    completed simple refferal bot with dict ram base

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 users = {
-    "1234": {"reffer_id": "5678", "flag": True},
-    "1235": {"reffer_id": "5678", "flag": True},
-    "5678": {"reffer_id": None, "flag": True},
-    "1237": {"reffer_id": "5678", "flag": True},
-    "1238": {"reffer_id": "5678", "flag": True},
-    "1239": {"reffer_id": "5678", "flag": True}
+    "1234": {"reffer_id": "1184193890", "flag": True, "name": "bot"},
+    "1235": {"reffer_id": "1184193890", "flag": True, "name": "bot"},
+    "5678": {"reffer_id": None, "flag": True, "name": "bot"},
+    "1237": {"reffer_id": "5678", "flag": True, "name": "bot"},
+    "1238": {"reffer_id": "5678", "flag": True, "name": "bot"},
+    "1239": {"reffer_id": "5678", "flag": True, "name": "bot"}
 }
 def get_user_ball(user_id):
         ball = 0
@@ -12,3 +12,5 @@ def get_user_ball(user_id):
             if user_id == user['reffer_id'] and user['flag']: ball += 1
         # print(f"{user} => {ball} ball")
         return ball
+
+# print(get_user_ball("1184193890"))
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from asyncio import run
-
+from aiogram.filters import CommandStart
+import functions
+import states
 dp = Dispatcher()
 
 async def startup_answer(bot: Bot):
-    await bot.send_message(1184193890, "Bot Ishga tushdi! ✅")
+    await bot.send_message("admin_id", "Bot Ishga tushdi! ✅")
 
 async def shutdown_answer(bot: Bot):
-    await bot.send_message(1184193890, "Bot ishdan to'xtadi! ❌")
+    await bot.send_message("admin_id", "Bot ishdan to'xtadi! ❌")
 
 async def start():
     dp.startup.register(startup_answer)
-
+    dp.message.register(functions.start_command_answer, CommandStart())
+    dp.message.register(functions.get_user_name_answer, states.get_user_name.name)
+    dp.message.register(functions.get_ref_link_answer, F.text == "Referal havola")
+    dp.message.register(functions.get_user_ball_answer, F.text == "Mening ballarim")
     dp.shutdown.register(shutdown_answer)
-    bot = Bot("6803882554:AAH91YHRcmFQQnZmhzy0ZWpRXscZiJBNeV4")
+    bot = Bot("bot_token")
     await dp.start_polling(bot)
 
 run(start())
